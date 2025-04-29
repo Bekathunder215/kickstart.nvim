ca9622fc5471e6d57f38b167adf5327484d4e52c
Bekathunder215 <johnbekiaris13@gmail.com>
2025-04-29 15:45:25 +0200

    disables copilot at startup

--- init.lua
+++ init.lua
@@ -1067,5 +1067,6 @@ vim.api.nvim_create_autocmd('BufEnter', {
   command = 'lcd %:p:h',
 })
 vim.o.clipboard = 'unnamedplus'
+vim.cmd 'Copilot disable'
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
